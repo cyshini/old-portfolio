ad8c906252aa29c7945e38fe48c7517588cbeb21
cyshini <francois.vilaire@gmail.com>
2016-07-16 16:49:53 +0200

    More fluid navigation between projects

--- config.rb
+++ config.rb
@@ -1,17 +1,6 @@
 # Reload the browser automatically whenever files change
 activate :livereload
 
-# Navtree for managing navigation easily
-activate :navtree do |options|
-  options.data_file = 'tree.yml' # The data file where our navtree is stored.
-  options.automatic_tree_updates = true # The tree.yml file will be updated automatically when source files are changed.
-  options.ignore_files = ['sitemap.xml', 'robots.txt', '404.html.erb'] # An array of files we want to ignore when building our tree.
-  options.ignore_dir = ['assets', 'views'] # An array of directories we want to ignore when building our tree.
-  options.home_title = 'Accueil' # The default link title of the home page (located at "/"), if otherwise not detected.
-  options.promote_files = ['index.html.erb'] # Any files we might want to promote to the front of our navigation
-  options.ext_whitelist = [] # If you add extensions (like '.md') to this array, it builds a whitelist of filetypes for inclusion in the navtree.
-end
-
 # Prefixes for browsers CSS compatibility
 activate :autoprefixer, browsers: ['last 2 versions', 'ie 8', 'ie 9']
 
--- source/assets/javascripts/application.js
+++ source/assets/javascripts/application.js
@@ -1,22 +1,11 @@
+//= require stickyfill.min.js
 //= require retina.min.js
 //= require smoothscroll.min.js
 //= require wow.min.js
 //= require barba.min.js
 
-smoothScroll.init({
-    selector: '[data-scroll]', // Selector for links (must be a valid CSS selector)
-    selectorHeader: '[data-scroll-header]', // Selector for fixed headers (must be a valid CSS selector)
-    speed: 500, // Integer. How fast to complete the scroll in milliseconds
-    easing: 'easeInOutCubic', // Easing pattern to use
-    offset: 50, // Integer. How far to offset the scrolling anchor location in pixels
-    updateURL: false, // Boolean. If true, update the URL hash on scroll
-    callback: function ( anchor, toggle ) {} // Function to run after scrolling
-});
-
-new WOW().init();
-
+/* Ajax loading for pages */
 Barba.Pjax.start();
-Barba.Pjax.fetch();
 
 var HideShowTransition = Barba.BaseTransition.extend({
   start: function() {
@@ -32,3 +21,23 @@ var HideShowTransition = Barba.BaseTransition.extend({
 Barba.Pjax.getTransition = function() {
   return HideShowTransition;
 };
+
+/* Smooth scrolling */
+smoothScroll.init({
+    selector: '[data-scroll]', // Selector for links (must be a valid CSS selector)
+    selectorHeader: '[data-scroll-header]', // Selector for fixed headers (must be a valid CSS selector)
+    speed: 500, // Integer. How fast to complete the scroll in milliseconds
+    easing: 'easeInOutCubic', // Easing pattern to use
+    offset: 50, // Integer. How far to offset the scrolling anchor location in pixels
+    updateURL: false, // Boolean. If true, update the URL hash on scroll
+    callback: function ( anchor, toggle ) {} // Function to run after scrolling
+});
+
+/* Animated elements on scroll */
+new WOW().init();
+
+/* Sticky sidebar in projects pages */
+Barba.Dispatcher.on('transitionCompleted', function() {
+  var sidebar = document.getElementById('sidebar');
+  Stickyfill.add(sidebar);
+});
