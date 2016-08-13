d699f193112c6d9ea969a38315312e620d5e9ab6
cyshini <francois.vilaire@gmail.com>
2016-08-14 01:13:48 +0200

    New images treatment

--- source/assets/javascripts/application.js
+++ source/assets/javascripts/application.js
@@ -1,11 +1,21 @@
 //= require blazy.min.js
+//= require sloth.min.js
 //= require smoothscroll.min.js
-//= require wow.min.js
+//= require retina.min.js
 
 /* Lazy loading */
 ;(function() {
   var bLazy = new Blazy();
 })();
+
+/* Initialize components on scroll */
+sloth({
+  on: document.querySelectorAll('.work'),
+  threshold: -200,
+  callback: function(element){
+    element.className += ' visible';
+  }
+});
 
 /* Smooth scrolling */
 smoothScroll.init({
@@ -17,6 +27,3 @@ smoothScroll.init({
     updateURL: false,
     callback: function ( anchor, toggle ) {}
 });
-
-/* Animated elements on scroll */
-new WOW().init();
